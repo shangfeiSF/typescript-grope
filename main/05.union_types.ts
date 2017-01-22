6af01fb3a376d81d8d03aaa2b9d33dfc70457883
shangfeiSF <tony20100125@126.com>
2017-01-22 17:06:32 +0800

    learn union types

---
+++ main/05.union_types.ts
@@ -0,0 +1,21 @@
+// 联合类型使用 | 分隔每个类型
+let isStringOrNumber: string | number;
+isStringOrNumber = 'hello world';
+isStringOrNumber = 7;
+isStringOrNumber = true;
+
+// 当不确定一个联合类型的变量是哪个类型时，只能访问此联合类型的所有类型里共有的属性或方法
+function useLength(params: string | number): number {
+    return params.length;
+}
+
+function useToString(params: string | number): string {
+    return params.toString();
+}
+
+// 联合类型的变量在被赋值的时候，会根据类型推论的规则推断出一个类型
+let testLength: string | number;
+testLength = 'hello world';
+console.log(testLength.length);
+testLength = 7;
+console.log(testLength.length);
