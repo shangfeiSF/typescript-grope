516d077f8804735e5511e20671390511ec7c09ea
shangfeiSF <tony20100125@126.com>
2017-01-23 17:32:31 +0800

    classes show how to use classes in base

--- main/07.classes.ts
+++ main/07.classes.ts
@@ -22,7 +22,7 @@ class Animal {
         this.name = name;
     }
 
-    move(meters: number = 0) {
+    move(meters: number = 0): void {
         console.log(`${this.name} moved ${meters}m.`);
     }
 }
@@ -33,21 +33,24 @@ class Dog extends Animal {
         super(name);
     }
 
-    move(meters: number = 5) {
+    move(meters: number = 5): void {
         console.log('Begin to run...');
         super.move(meters);
     }
 }
 
-// 可以重写继承自基类的方法，也可以调用继承自基类的方法
+// 可以重写继承自基类的方法
+// 可以通过this或者super来调用继承自基类的方法
 class Lion extends Animal {
     constructor(name: string) {
         super(name);
     }
 
-    move(meters: number = 10) {
+    move(meters: number = 10): void {
         console.log('Begin to hunt...');
         super.move(meters);
+        console.log('Begin to hunt again...');
+        this.move(meters)
     }
 }
 
@@ -57,94 +60,91 @@ dog.move(20);
 let lion = new Lion('sinba');
 lion.move(50);
 
-// TypeScript的类具有public、private、protected修饰符
-
-// public修饰符是默认的
-class Car {
-    public model;
-
-    constructor(model: string) {
-        this.model = model;
-    }
-
-    move(meters: number) {
-        console.log(`${this.model} car moved ${meters}`);
-    }
-}
-
-var car = new Car('Ford');
-console.log(car.model);
-car.move(1000);
-
-// private修饰符限制属性或方法只能在类内部使用
+// readyonly修饰符将属性设置为只读的。 只读属性必须在声明时或构造函数里被初始化
 class Phone {
-    private password: number;
+    readonly productionID: string = 'abcd123456';
+    readonly phoneNumber: number;
 
-    constructor(password) {
-        this.password = password;
-    }
-
-    private showPassword() {
-        console.log(`The password of phone is ${this.password}`)
-    }
-
-    open(someone: string, password: number) {
-        if (this.password === password) {
-            this.showPassword()
-            console.log(`${someone} opened this phone.`)
-            return true;
-        } else {
-            console.log(`${someone} did not open this phone.`)
-            return false;
-        }
+    constructor(phoneNumber: number) {
+        this.phoneNumber = phoneNumber;
     }
 }
 
-let phone = new Phone(123456);
-console.log(phone.password);
-phone.showPassword();
-phone.open('shangfei', 123456);
-phone.open('shangfet', 654321);
+let phone = new Phone(12345678901);
+console.log(phone.productionID);
+console.log(phone.phoneNumber);
+phone.productionID = '123456abcd';
+phone.phoneNumber = 10987654321;
 
-// private修饰符限制的属性和方法在派生类中无法访问
-class MobilePhone extends Phone {
-    private size: string;
-
-    constructor(password: number, size: string) {
-        super(password);
-        this.size = size;
+// 参数属性：可以方便地让我们在一个地方定义并初始化一个属性
+class Engineer {
+    constructor(public name: string, private phone: number, protected id: number) {
     }
 
-    showSize() {
-        console.log(`The size of mobilePhone is ${this.size}.`);
-    }
-
-    selfShowPassword() {
-        console.log(this.password);
-        console.log(super.password);
-        this.showPassword();
-        super.showPassword();
-    }
-
-    call(someone: string, password: number) {
-        let open = this.open(someone, password)
-        if (open) {
-            console.log(`${someone} opened this mobilePhone.`);
-            console.log(`${someone} called with this mobilePhone.`);
-            return true;
-        } else {
-            console.log(`${someone} did not open this mobilePhone.`);
-            console.log(`${someone} did not call with this mobilePhone.`);
-            return false;
-        }
+    info(): void {
+        console.log(`Name: ${this.name}`);
+        console.log(`Phone: ${this.phone}`);
+        console.log(`Id: ${this.id}`);
     }
 }
 
-let mobilePhone = new MobilePhone(123456, '4.5inch');
-console.log(mobilePhone.size);
-console.log(mobilePhone.password);
-mobilePhone.showPassword();
-mobilePhone.selfShowPassword();
-mobilePhone.showSize();
-mobilePhone.call('shangfei', 123456);
-mobilePhone.call('shangfei', 654321);
+let engineer = new Engineer('shangfei', 12345678901, 1);
+engineer.info();
+console.log(engineer.name);
+console.log(engineer.phone);
+console.log(engineer.id);
+
+// 类的实例成员，仅当类被实例化的时候才会被初始化
+// 类的静态成员，存在于类本身上而不是类的实例上
+class Grid {
+    static originX: number = 0;
+    static originY: number = 0;
+
+    constructor(public scale: number) {
+    }
+
+    distance(point: {x: number; y: number;}): number {
+        let xDist = (point.x - Grid.originX);
+        let yDist = (point.y - Grid.originY);
+        return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+    }
+}
+
+let grid1 = new Grid(1.0);
+let grid2 = new Grid(5.0);
+
+console.log(grid1.distance({x: 10, y: 10}));
+console.log(grid2.distance({x: 10, y: 10}));
+
+// 定义类的同时也定义了实例类型和类的类型
+// class Cup定义了实例类型Cup和类的类型typeof Cup
+class Cup {
+    static description: string = 'Cup is used to drink.';
+
+    constructor(public material: string) {
+    }
+}
+
+console.log(Cup.description);
+let cup: Cup;
+cup = new Cup('glass');
+
+let CupAlias: typeof Cup = Cup;
+
+console.log(CupAlias.description);
+let anotherCup: Cup;
+anotherCup = new CupAlias('iron');
+
+// 类作为接口使用
+// 因为类可以创建出类型，所以你能够在允许使用接口的地方使用类
+
+class Point {
+    x: number;
+    y: number;
+}
+
+interface Point3d extends Point {
+    z: number;
+}
+
+let point3d: Point3d = {x: 1, y: 2, z: 3};
